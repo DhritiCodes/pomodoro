a9ef11fa47ff1dc528bc8d7e760b1b95c10344c8
Dhriti <dhritideepagorai@gmail.com>
2022-03-31 08:56:09 +0530

    button functionality, audio effect added

--- pomodoro.js
+++ pomodoro.js
@@ -1,4 +1,3 @@
-const longPomodoro = document.querySelector('.long-pmdr');
 const shortPomodoro = document.querySelector('.short-pmdr');
 const pmdrTimer = document.querySelector('.ongoing-timer');
 //toggle b/w screens using display(none/inline) to hide objects 
@@ -14,70 +13,20 @@ var startingMinutes, minutes, seconds;
 
 var timerInterval;
 
-// 50 min pomodoro
-longPomodoro.addEventListener('click', function() {
-    startingMinutes = 50;
-    time = startingMinutes * 60;
-
-    //switch to timer window
-    first.style.display = 'none';
-    second.style.display = 'inline';
-
-    //pause button should be displayed at first -  not resume button 
-    resume.style.display = "none"
-    pause.style.display = "inline";
-    console.log(time);
-
-    //finish button should be displayed first, not start break button
-    finish.style.display = "inline";
-    breakBtn.style.display = "none";
-
-    timerInterval = setInterval(() => {
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        pmdrTimer.innerHTML = `${minutes}:${seconds}`;
-        time--;
-        //stop when timer ends
-        if (time < 0) {
-            clearInterval(timerInterval);
-            //ADD FINISHING EFFECTS + AUDIO
 
 
-            return;
-        }
-        // console.log(time);
-        //CHANGE 40 => 1000 I.E. 
-    }, 1000);
-})
+startingMinutes = 25;
+time = startingMinutes * 60;
 
 //25 min pomodoro
 shortPomodoro.addEventListener('click', function() {
-    startingMinutes = 1;
-    time = startingMinutes * 60;
 
     //switch to timer window 
     first.style.display = "none";
     second.style.display = "inline";
 
-    timerInterval = setInterval(() => {
-        var minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        pmdrTimer.innerHTML = `${minutes}:${seconds}`;
-        time--;
-        //stop when timer ends
-        if (time < 0) {
-            clearInterval(timerInterval);
-            //ADD FINISHING EFFECTS + AUDIO
-
-
-            return;
-        }
-        // console.log(time);
-    }, 500)
+    executeTimer();
 })
-
 
 //adding button functionality
 
@@ -86,6 +35,8 @@ reset.addEventListener('click', () => {
     clearInterval(timerInterval);
     second.style.display = 'none';
     first.style.display = 'inline';
+    startingMinutes = 25;
+    time = startingMinutes * 60;
 })
 
 //PAUSE BUTTON
@@ -97,28 +48,14 @@ pause.addEventListener('click', () => {
     // timerInterval
     clearInterval(timerInterval);
     pmdrTimer.innerHTML = `${Math.floor(time / 60)}:${time%60}`;
-
+    console.log(time);
 })
 
+//RESUME BUTTON
 resume.addEventListener('click', () => {
     console.log("resume");
 
-    timerInterval = setInterval(() => {
-        var minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        pmdrTimer.innerHTML = `${minutes}:${seconds}`;
-        time--;
-        //stop when timer ends
-        if (time < 0) {
-            clearInterval(timerInterval);
-            //ADD FINISHING EFFECTS + AUDIO
-
-
-            return;
-        }
-        // console.log(time);
-    }, 1000)
+    executeTimer();
 
     resume.style.display = "none"
     pause.style.display = "inline";
@@ -133,10 +70,19 @@ finish.addEventListener("click", () => {
     breakBtn.style.display = "inline";
     finish.style.display = "none";
 
-    //disable pause and resume button after finish
-    // resume.disabled = "true";
-    // pause.disabled = "true"
+    //when finished, options -  start break or reset timer, DISABLE RESUME AND PAUSE BUTTON, AGAIN TURN DISABILITY OFF WHEN STARTING POMODORO
+    pause.disabled = true;
+    resume.disabled = true;
+    startingMinutes = 25;
+    time = startingMinutes * 60;
+
+    // //break-finish  audio
+    // var audio = new Audio('break-finish.mp3');
+    // audio.play();
+
+    console.log("finished");
 })
+
 
 breakBtn.addEventListener("click", () => {
     //display finish button again
@@ -147,21 +93,42 @@ breakBtn.addEventListener("click", () => {
     //hardcoded break of 5mins for now ---CHANGE LATER
     startingMinutes = 5;
     time = startingMinutes * 60;
+    executeTimer();
 
+})
+
+//functions 
+
+// 1.EXECUTE TIMER 
+function executeTimer() {
     timerInterval = setInterval(() => {
         var minutes = Math.floor(time / 60);
         let seconds = time % 60;
         seconds = seconds < 10 ? '0' + seconds : seconds;
         pmdrTimer.innerHTML = `${minutes}:${seconds}`;
         time--;
-        //stop when timer ends
+        console.log(time)
+            //stop when timer ends
         if (time < 0) {
             clearInterval(timerInterval);
             //ADD FINISHING EFFECTS + AUDIO
-            // console.log("COngratulations, u completed ur goal")
-
+            var audio = new Audio('timer-finish.mp3');
+            audio.play();
             return;
         }
-        // console.log(time);
+
+        //REMOVE DISABILITY WHICH WAS ENABLED WHEN POMODORO "FINISHED"
+        pause.disabled = false;
+        resume.disabled = false;
+
+        breakBtn.style.display = "none";
+        finish.style.display = "inline";
+        // finish.innerHTML = "FINISH POMODORO"
+
+        resume.style.display = "none"
+        pause.style.display = "inline";
+
     }, 1000)
-})
+}
+
+// function animation
